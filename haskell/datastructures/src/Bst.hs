337230c4bb10a720b0fd4539e05e51150d6cc6f8
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2023-02-26 16:43:20 +0300

    Worked on bst tests

--- haskell/datastructures/src/Bst.hs
+++ haskell/datastructures/src/Bst.hs
@@ -1,15 +1,35 @@
-module Bst(push, contains, wrap) where
+module Bst (push, contains, wrap, Tree (..)) where
+
+import Control.Monad
+import Test.QuickCheck
+
+import System.Random(Random)
 
 data Tree a = Empty | Node (Tree a) a (Tree a)
-  deriving (Functor, Foldable, Traversable, Show)
+  deriving (Functor, Foldable, Traversable, Show, Eq)
 
 instance Ord a => Semigroup (Tree a) where
- -- (<>) :: Tree a -> Tree a -> Tree a
- (<>) Empty treeb = treeb
- (<>) treea treeb = foldl push treeb treea
+  -- (<>) :: Tree a -> Tree a -> Tree a
+  (<>) Empty treeb = treeb
+  (<>) treea treeb = foldl push treeb treea
 
 instance Ord a => Monoid (Tree a) where
   mempty = Empty
+
+instance (Ord a, Arbitrary a, Num a, Random a) => Arbitrary (Tree a) where
+  arbitrary = gen 0 100
+    where
+      gen :: (Ord a, Num a, Random a) => a -> a -> Gen (Tree a)
+      gen minVal maxVal
+        | maxVal - minVal < 3 = return Empty
+        | otherwise = do
+            val <- choose (minVal, maxVal)
+            liftM3 Node (gen minVal (val - 1)) (return val) (gen (val + 1) maxVal)
+
+toList 
+instance Eq a => Eq (Tree a) where
+  eq a b = 
+
 
 push :: Ord a => Tree a -> a -> Tree a
 push Empty a = Node Empty a Empty
@@ -26,4 +46,3 @@ contains (Node left value right) a
 
 wrap :: a -> Tree a
 wrap a = Node Empty a Empty
-
--- haskell/datastructures/test/BstTest.hs
+++ haskell/datastructures/test/BstTest.hs
@@ -1,27 +1,42 @@
-module BstTest(main) where
-import Test.QuickCheck
-import Data.List(sort)
+module BstTest (main) where
 
-import qualified Bst
+import Bst (Tree (..), wrap)
+import Data.List (sort)
+import Test.QuickCheck
+import Test.QuickCheck.Property.Monoid (T (..), eq, prop_Monoid)
 
 prop_int_binary_search_tree_built_correctly :: [Int] -> Bool
 prop_int_binary_search_tree_built_correctly is = sort is == js
-  where tree = mconcat (fmap Bst.wrap is)
-        js = foldr (:) [] tree
+  where
+    tree = mconcat (fmap wrap is)
+    js = foldr (:) [] tree
 
 prop_str_binary_search_tree_built_correctly :: [String] -> Bool
 prop_str_binary_search_tree_built_correctly is = sort is == js
-  where tree = mconcat (fmap Bst.wrap is)
-        js = foldr (:) [] tree
+  where
+    tree = mconcat (fmap wrap is)
+    js = foldr (:) [] tree
 
 prop_int_binary_search_tree_monoid :: [Int] -> [Int] -> Bool
 prop_int_binary_search_tree_monoid is js = sort (is ++ js) == ks
-  where itree = mconcat (fmap Bst.wrap is)
-        jtree = mconcat (fmap Bst.wrap js)
-        tree = itree <> jtree
-        ks = foldr (:) [] tree
+  where
+    itree = mconcat (fmap wrap is)
+    jtree = mconcat (fmap wrap js)
+    tree = itree <> jtree
+    ks = foldr (:) [] tree
+
+prop_int_binary_search_tree_monoid_2 :: [Int] -> [Int] -> Bool
+prop_int_binary_search_tree_monoid_2 is js = sort (is ++ js) == ks
+  where
+    itree = mconcat (fmap wrap is)
+    jtree = mconcat (fmap wrap js)
+    tree = itree <> jtree
+    ks = foldr (:) [] tree
+
 main :: IO ()
 main = do
   quickCheck prop_int_binary_search_tree_built_correctly
   quickCheck prop_str_binary_search_tree_built_correctly
   quickCheck prop_int_binary_search_tree_monoid
+  quickCheck prop_int_binary_search_tree_monoid_2
+  quickCheck $ eq $ prop_Monoid (T :: T (Tree Int))
